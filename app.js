3dbfbf3497f62022ad1d793c29879a13e092e418
Marek Petr <marek.p4@seznam.cz>
2020-03-07 22:04:37 +0100

    Přišel jsem na to, proč se nezobrazovaly ikonky. Vyřešeno 8-)

--- app.js
+++ app.js
@@ -14,3 +14,29 @@ const options = {weekday: "long", month: "long", day:"numeric"};
 const today = new Date();
 
 dateElement.innerHTML = today.toLocaleDateString("cs-CZ",options);
+
+// add to-do function
+function addToDo(toDo){
+    const item = `
+                    <li class="item">
+                        <i class="fa fa-circle-thin co" job="complete" id="0"></i>
+                        <p class="text">Drink coffe</p>
+                        <i class="fa fa-trash-o de" job="delete" id="0"></i>
+                    </li>
+                    `;
+    const position = "beforeend";
+
+    list.insertAdjacentHTML(position, item);
+}
+
+//add an item to the list user the enter key
+document.addEventListener("keyup", function(even){
+    if(event.keyCode == 13){
+        const toDo = input.nodeValue;
+
+        //if the input isn't empty
+        if(toDo){
+            addToDo(toDo);
+        }
+    }
+})
